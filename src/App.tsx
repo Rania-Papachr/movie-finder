2143870a462ca080063710a7965cf7d295f69745
Rania-Papachr <raniapap1371@gmail.com>
2026-04-30 16:51:00 +0300

    feat: adding POST, PUT ,DELETE and GETby id endpoints on backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 type GenreTypes = "comedy" | "horror" | "drama";
 
@@ -18,14 +19,16 @@ function App() {
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    fetch("http://localhost:5000")
+    axios
+      .get("http://localhost:5000")
       .then((res) => res.json())
       .then((data) => setMessage(data.message))
       .catch((err) => console.error("Error fetching API:", err));
   }, []);
 
   useEffect(() => {
-    fetch("http://localhost:5000/movies")
+    axios
+      .get("http://localhost:5000/movies")
       .then((res) => res.json())
       .then((data) => {
         setMovies(data);
@@ -36,6 +39,56 @@ function App() {
         return console.error("Error fetching API:", err);
       });
   }, []);
+
+  // useEffect(() => {
+  //   axios
+  //     .get("http://localhost:5000/movies/1")
+  //     .then((res) => setMovie(res.data))
+  //     .catch((err) => console.error(err));
+  // }, []);
+
+  // const addMovie = async () => {
+  //   try {
+  //     const res = await axios.post("http://localhost:5000/movies", {
+  //       title: "Gladiator",
+  //       year: 2000,
+  //       description: "A Roman general seeks revenge.",
+  //       rating: 8.5,
+  //       genre: "drama",
+  //       imageUrl: "https://via.placeholder.com/300x450?text=Gladiator",
+  //     });
+
+  //     console.log("Created:", res.data);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
+
+  // const updateMovie = async () => {
+  //   try {
+  //     const res = await axios.put("http://localhost:5000/movies/1", {
+  //       title: "Inception UPDATED",
+  //       year: 2010,
+  //       description: "Dream within a dream.",
+  //       rating: 9.0,
+  //       genre: "drama",
+  //       imageUrl: "https://via.placeholder.com/300x450?text=Inception",
+  //     });
+
+  //     console.log(res.data);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
+
+  // const deleteMovie = async (id: number) => {
+  //   try {
+  //     const res = await axios.delete(`http://localhost:5000/movies/${id}`);
+  //     console.log(res.data);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
 
   return (
     <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
